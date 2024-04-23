fdff9bd90454ec12174160b4771e4b2b31c1e2ca
Weronika Brzeczkowska-Kuzianik <brzeczkowska.w@gmail.com>
2024-04-23 22:15:43 +0200

    setup project

---
+++ src/pages/Create.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>WELCOME TO THE create PAGE</h1>
+</template>
+
+<script>
+export default {
+    // Component functionality
+}
+</script>
+
+<style>
+    /* Component styling */
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../pages/Home.vue';
+import Create from '../pages/Create.vue';
+import About from '../pages/About.vue';
+import Detail from '../pages/Detail.vue';
+
+const routes = [
+    {    
+        path: '/',
+        component: Home
+    },
+    {
+        path: '/create',
+        component: Create
+    },
+    {
+        path: '/about',
+        component: About
+    },
+    {
+        path: '/detail',
+        component: Detail
+    }
+];
+
+const router = createRouter({
+    scrollBehavior: function() {
+        return { x: 0, y: 0 };
+    },
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
